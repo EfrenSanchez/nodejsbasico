316f50a50ad700f304157c2180f5cd023e06427c
Efren Sanchez <efren.sanchezgon@gmail.com>
2018-12-05 13:28:10 +0100

    Implemented http server

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require ('http');
+const fs = require ('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res)=>{
+
+  fs.readFile('<./src/path>', (err, data)=>{
+
+    if (err) { console.error(err); return; }
+
+    //responses:
+
+    res.statusCode = 200;
+
+    res.end(data);
+
+  });
+
+});
+
+
+//Listening for requests
+
+server.listen(port, ()=>{
+
+  console.log(`server listening on ${port}...`);
+
+});
