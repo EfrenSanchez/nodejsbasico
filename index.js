5b2b71b1e1ceca66f1bbb03b8904e5058c87239a
Efren Sanchez <efren.sanchezgon@gmail.com>
2018-12-05 13:34:33 +0100

    completed

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require("fs");
+const fops = require ('./src/fileops');
+
+//path del archivo a leer
+let read_path = '<./resources/number.txt>'
+
+//path donde se guardará el archivo escrito
+let write_path = '<./resources/new_number.txt>'
+
+
+fs.readFile(read_path, "utf8",(error,result) => {
+    if(error) throw error;
+    let newText = fops.incrementValues(result);
+    fs.writeFile(write_path, newText,(error,result)=>{
+        if(error) throw error;
+        console.log("script creado");
+    });
+});
