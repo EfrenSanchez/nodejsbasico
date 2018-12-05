ba6ff4177c76d4036fcaf90197aff4846776badb
Efren Sanchez <efren.sanchezgon@gmail.com>
2018-12-05 13:37:27 +0100

    complated

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const exec = require("child_process").exec;
+const readline = require ('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function execCommand(command){
+  exec(command,
+    (err, stdout, stderr) => {
+      if (err) throw console.log(err);
+      if (stdout) console.log(`Standard out: \n${stdout}`);
+      if (stderr) console.log(`Standard err: \n${stderr}`);
+    });
+};
+
+rl.question('>', (answer)=>{
+  execCommand(answer);
+  rl.close();
+});
